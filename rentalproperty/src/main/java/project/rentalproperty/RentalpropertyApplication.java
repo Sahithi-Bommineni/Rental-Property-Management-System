70d1b06139edea716d42d3b3b41a03aef258824d
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-04-14 17:32:07 -0500

    generated spring initializer

---
+++ rentalproperty/src/main/java/project/rentalproperty/RentalpropertyApplication.java
@@ -0,0 +1,13 @@
+package project.rentalproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalpropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalpropertyApplication.class, args);
+	}
+
+}
---
+++ rentalproperty/src/test/java/project/rentalproperty/RentalpropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package project.rentalproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalpropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
