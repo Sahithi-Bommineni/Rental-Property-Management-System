b8a9165151fc716e8f949dea5fd44e2a8cab4c64
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-04-17 00:20:21 -0500

    owner home page

--- rentalproperty/src/main/java/project/rentalproperty/controller/UserController.java
+++ rentalproperty/src/main/java/project/rentalproperty/controller/UserController.java
@@ -3,15 +3,21 @@ package project.rentalproperty.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class UserController {
 
     @GetMapping("/login")
-    public String getMethodName(Model model) {
+    public String login(Model model) {
         return "login";
     }
     
+    @GetMapping("/ownerhome")
+    public String ownherlogin(Model model) {
+        return "ownerhome";
+    }
     
 }
